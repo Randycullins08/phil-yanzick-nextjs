4ef364e4313c4c284d6b365e843b91e94b4db5fc
Randy Cullins <randycullins2018@gmail.com>
2023-01-26 18:58:43 -0700

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/main.scss'
+import { Analytics } from "@vercel/analytics/react";
+
+import "../styles/main.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
