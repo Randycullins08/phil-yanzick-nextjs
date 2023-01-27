3215e052cc057b4b52e226903c9aa0bd44c1a642
Randy Cullins <randycullins2018@gmail.com>
2023-01-27 08:50:36 -0700

    added new album to portfolio

--- data/albumData.js
+++ data/albumData.js
@@ -1,13 +1,13 @@
 export const albumData = [
   {
     id: 0,
-    bandName: "Hibernator",
+    bandName: "NVM",
     work: "Engineered",
     player: (
       <iframe
-        title="Hibernator"
+        title="NVM"
         style={{ border: "0", width: "350px", height: "470px" }}
-        src="https://open.spotify.com/embed/album/3rFCWGQn6WxEKLna1htaty?utm_source=generator"
+        src="https://open.spotify.com/embed/album/3cIrdBgxHkf6o65enOmvTO?utm_source=generator"
       />
     ),
   },
@@ -19,19 +19,19 @@ export const albumData = [
       <iframe
         title="Hibernator"
         style={{ border: "0", width: "350px", height: "470px" }}
-        src="https://open.spotify.com/embed/album/7xuPbwcZrj3diallD0lBXk?utm_source=generator"
+        src="https://open.spotify.com/embed/album/3rFCWGQn6WxEKLna1htaty?utm_source=generator"
       />
     ),
   },
   {
     id: 2,
-    bandName: "NVM",
+    bandName: "Hibernator",
     work: "Engineered",
     player: (
       <iframe
-        title="NVM"
+        title="Hibernator"
         style={{ border: "0", width: "350px", height: "470px" }}
-        src="https://open.spotify.com/embed/album/61yrnIQNoSMirv6gadFV6W?utm_source=generator"
+        src="https://open.spotify.com/embed/album/7xuPbwcZrj3diallD0lBXk?utm_source=generator"
       />
     ),
   },
@@ -43,12 +43,24 @@ export const albumData = [
       <iframe
         title="NVM"
         style={{ border: "0", width: "350px", height: "470px" }}
-        src="https://open.spotify.com/embed/album/5IXMoep5wF4BBJmq6YIXDm?utm_source=generator"
+        src="https://open.spotify.com/embed/album/61yrnIQNoSMirv6gadFV6W?utm_source=generator"
       />
     ),
   },
   {
     id: 4,
+    bandName: "NVM",
+    work: "Engineered",
+    player: (
+      <iframe
+        title="NVM"
+        style={{ border: "0", width: "350px", height: "470px" }}
+        src="https://open.spotify.com/embed/album/5IXMoep5wF4BBJmq6YIXDm?utm_source=generator"
+      />
+    ),
+  },
+  {
+    id: 5,
     bandName: "Hey ILY",
     work: "Engineered Drums | Mixed",
     player: (
@@ -60,7 +72,7 @@ export const albumData = [
     ),
   },
   {
-    id: 5,
+    id: 6,
     bandName: "Anaconda Vise",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -72,7 +84,7 @@ export const albumData = [
     ),
   },
   {
-    id: 6,
+    id: 7,
     bandName: "Chairea",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -84,7 +96,7 @@ export const albumData = [
     ),
   },
   {
-    id: 7,
+    id: 8,
     bandName: "BWB Artist",
     work: "Guitars | Bass | Drums | Keys | Engineered | Mixed | Mastered",
     player: (
@@ -96,7 +108,7 @@ export const albumData = [
     ),
   },
   {
-    id: 8,
+    id: 9,
     bandName: "Chairea",
     work: "Engineered | Mixed",
     player: (
@@ -108,7 +120,7 @@ export const albumData = [
     ),
   },
   {
-    id: 9,
+    id: 10,
     bandName: "Treasure State",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -120,7 +132,7 @@ export const albumData = [
     ),
   },
   {
-    id: 10,
+    id: 11,
     bandName: "Anaconda Vise",
     work: "Live Recorded | Mixed | Mastered",
     player: (
@@ -132,7 +144,7 @@ export const albumData = [
     ),
   },
   {
-    id: 11,
+    id: 12,
     bandName: "NVM",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -144,7 +156,7 @@ export const albumData = [
     ),
   },
   {
-    id: 12,
+    id: 13,
     bandName: "Gray Joy",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -156,7 +168,7 @@ export const albumData = [
     ),
   },
   {
-    id: 13,
+    id: 14,
     bandName: "Dragged Out",
     work: "Engineered",
     player: (
@@ -168,7 +180,7 @@ export const albumData = [
     ),
   },
   {
-    id: 14,
+    id: 15,
     bandName: "Sky Drftr",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -180,7 +192,7 @@ export const albumData = [
     ),
   },
   {
-    id: 15,
+    id: 16,
     bandName: "Tom Russell",
     work: "Vocal Engineered",
     player: (
@@ -192,7 +204,7 @@ export const albumData = [
     ),
   },
   {
-    id: 16,
+    id: 17,
     bandName: "Hubba Hubba",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -204,7 +216,7 @@ export const albumData = [
     ),
   },
   {
-    id: 17,
+    id: 18,
     bandName: "Hubba Hubba",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -216,7 +228,7 @@ export const albumData = [
     ),
   },
   {
-    id: 18,
+    id: 19,
     bandName: "Tiny Iota",
     work: "Mixed | Mastered",
     player: (
