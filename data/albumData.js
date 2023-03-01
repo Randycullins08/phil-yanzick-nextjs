6e278f43b2bc64ca2c4a2756a0f9affce99b7093
Randy Cullins <randycullins2018@gmail.com>
2023-02-28 17:00:59 -0700

    fixed ids for albumData

--- data/albumData.js
+++ data/albumData.js
@@ -12,7 +12,7 @@ export const albumData = [
     ),
   },
   {
-    id: 0,
+    id: 1,
     bandName: "NVM",
     work: "Engineered",
     player: (
@@ -24,7 +24,7 @@ export const albumData = [
     ),
   },
   {
-    id: 1,
+    id: 2,
     bandName: "Hibernator",
     work: "Engineered",
     player: (
@@ -36,7 +36,7 @@ export const albumData = [
     ),
   },
   {
-    id: 2,
+    id: 3,
     bandName: "Hibernator",
     work: "Engineered",
     player: (
@@ -48,7 +48,7 @@ export const albumData = [
     ),
   },
   {
-    id: 3,
+    id: 4,
     bandName: "NVM",
     work: "Engineered",
     player: (
@@ -60,7 +60,7 @@ export const albumData = [
     ),
   },
   {
-    id: 4,
+    id: 5,
     bandName: "NVM",
     work: "Engineered",
     player: (
@@ -72,7 +72,7 @@ export const albumData = [
     ),
   },
   {
-    id: 5,
+    id: 6,
     bandName: "Hey ILY",
     work: "Engineered Drums | Mixed",
     player: (
@@ -84,7 +84,7 @@ export const albumData = [
     ),
   },
   {
-    id: 6,
+    id: 7,
     bandName: "Anaconda Vise",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -96,7 +96,7 @@ export const albumData = [
     ),
   },
   {
-    id: 7,
+    id: 8,
     bandName: "Chairea",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -108,7 +108,7 @@ export const albumData = [
     ),
   },
   {
-    id: 8,
+    id: 9,
     bandName: "BWB Artist",
     work: "Guitars | Bass | Drums | Keys | Engineered | Mixed | Mastered",
     player: (
@@ -120,7 +120,7 @@ export const albumData = [
     ),
   },
   {
-    id: 9,
+    id: 10,
     bandName: "Chairea",
     work: "Engineered | Mixed",
     player: (
@@ -132,7 +132,7 @@ export const albumData = [
     ),
   },
   {
-    id: 10,
+    id: 11,
     bandName: "Treasure State",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -144,7 +144,7 @@ export const albumData = [
     ),
   },
   {
-    id: 11,
+    id: 12,
     bandName: "Anaconda Vise",
     work: "Live Recorded | Mixed | Mastered",
     player: (
@@ -156,7 +156,7 @@ export const albumData = [
     ),
   },
   {
-    id: 12,
+    id: 13,
     bandName: "NVM",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -168,7 +168,7 @@ export const albumData = [
     ),
   },
   {
-    id: 13,
+    id: 14,
     bandName: "Gray Joy",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -180,7 +180,7 @@ export const albumData = [
     ),
   },
   {
-    id: 14,
+    id: 15,
     bandName: "Dragged Out",
     work: "Engineered",
     player: (
@@ -192,7 +192,7 @@ export const albumData = [
     ),
   },
   {
-    id: 15,
+    id: 16,
     bandName: "Sky Drftr",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -204,7 +204,7 @@ export const albumData = [
     ),
   },
   {
-    id: 16,
+    id: 17,
     bandName: "Tom Russell",
     work: "Vocal Engineered",
     player: (
@@ -216,7 +216,7 @@ export const albumData = [
     ),
   },
   {
-    id: 17,
+    id: 18,
     bandName: "Hubba Hubba",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -228,7 +228,7 @@ export const albumData = [
     ),
   },
   {
-    id: 18,
+    id: 19,
     bandName: "Hubba Hubba",
     work: "Engineered | Mixed | Mastered",
     player: (
@@ -240,7 +240,7 @@ export const albumData = [
     ),
   },
   {
-    id: 19,
+    id: 20,
     bandName: "Tiny Iota",
     work: "Mixed | Mastered",
     player: (
